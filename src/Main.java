252019b588a57e9d6c2ceced7ad3594a50115e6e
Grafskii <miti-h@yandex.ru>
2023-04-15 22:26:59 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket_price = 16240; // цена за билет
+
+        int price_per_mile = 20; // цена за одну милю
+
+        System.out.println((ticket_price / price_per_mile) + " миль");
+    }
+}
