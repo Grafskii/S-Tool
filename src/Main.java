7360708f81579a265a0657d704030b035291d264
Grafskii <miti-h@yandex.ru>
2023-04-20 15:00:35 +0300

    added

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticket_price = 16240; // цена за билет
+        int ticketPrice = 16240; // цена за билет
 
-        int price_per_mile = 20; // цена за одну милю
+        int pricePerMile = 20; // цена за одну милю
 
-        System.out.println((ticket_price / price_per_mile) + " миль");
+        System.out.println((ticketPrice / pricePerMile) + " миль");
     }
 }
